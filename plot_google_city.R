885e1fa18af1649f0c7f604ac3e4e8127afb3016
garrettthomas <gthomas@predsci.com>
2014-07-18 09:37:19 -0700

    plot_google_city better

--- plot_google_city.R
+++ plot_google_city.R
@@ -18,7 +18,7 @@ c <- 1
 for (i in which_cities){
   
   Colors = rainbow(length(which_cities))
-  plot(city[a:b,i], type = "l", col = Colors[c],ylim = g_range, axes = FALSE, ann = FALSE)
+  plot(city[a:b,i+1], type = "l", col = Colors[c], ylim = g_range, axes = FALSE, ann = FALSE)
   c = c+1
   par(new = T)
 }
@@ -29,7 +29,7 @@ title(ylab = "Cases", col.lab = rgb(0,0,0))
 title(main = "Cities", col.main="black", font.main=4)
 
      
-        legend("topright",names(city)[which_cities-2],lwd = c(2.5,2.5),col = Colors,cex=0.4, ncol=2)
+        legend("topright",names(city)[which_cities+1],lwd = c(2.5,2.5),col = Colors,cex=0.4, ncol=2)
        par(new = FALSE)
 }
 }
